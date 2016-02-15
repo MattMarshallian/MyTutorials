e85bdd022ad544d35d6248bac2e96ab88fe23a26
Matt Marshallian <matt.marshallian@gmail.com>
2016-02-16 00:32:16 +0100

    Inflate downloaded zip

--- MySaveDataFile/app/src/main/java/com/example/tutorial/marcin/mysavedatafile/MainActivity.java
+++ MySaveDataFile/app/src/main/java/com/example/tutorial/marcin/mysavedatafile/MainActivity.java
@@ -4,10 +4,10 @@ import android.Manifest;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -16,13 +16,19 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.zip.DataFormatException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -57,15 +63,69 @@ public class MainActivity extends AppCompatActivity {
 
                 new Thread(new Runnable() {
                     public void run() {
-                        downloadFile();
+
+                        if (isServerOnline())
+                        {
+                            showError("Serwer działa");
+                        }
+                        else
+                        {
+                            showError("Serwer NIE działa");
+                        }
+
+                        // downloadFile();
+
+                        try {
+                            inflateFile();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
                     }
                 }).start();
             }
         });
     }
 
+    private void inflateFile() throws Exception {
+        runOnUiThread(new Runnable() {
+            int totalSize = 300000;
+
+            public void run() {
+                pb.setMax(totalSize);
+            }
+        });
+
+        File SDCardRoot = Environment.getExternalStorageDirectory();
+        File file = new File(SDCardRoot,"downloaded_file.zip");
+
+
+        File theDir = new File(SDCardRoot, "HTML5");
+
+        String directoryName = "HTML5";
+
+        // if the directory does not exist, create it
+        if (!theDir.exists()) {
+            try{
+                theDir.mkdir();
+            }
+            catch(SecurityException se) { }
+        }
+
+        UnzipUtility unzipUtility = new UnzipUtility();
+        unzipUtility.unzip(file.getAbsolutePath(), file.getParent() + File.separatorChar + directoryName);
+
+        runOnUiThread(new Runnable() {
+            public void run() {
+                Log.d("DUPA", "Plik rozpakowany");
+                dialog.dismiss(); // if you want close it..
+            }
+        });
+    }
+
     public void downloadFile() {
         try {
+
             URL url = new URL(download_file_path);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 
@@ -78,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
             //set the path where we want to save the file
             File SDCardRoot = Environment.getExternalStorageDirectory();
             //create a new file, to save the downloaded file
-            File file = new File(SDCardRoot,"downloaded_file.png");
+            File file = new File(SDCardRoot,"downloaded_file.zip");
 
             FileOutputStream fileOutput = new FileOutputStream(file);
 
@@ -102,7 +162,7 @@ public class MainActivity extends AppCompatActivity {
                 fileOutput.write(buffer, 0, bufferLength);
                 downloadedSize += bufferLength;
 
-                // update the progressbar //
+                // update the progressbar
                 runOnUiThread(new Runnable() {
                     public void run() {
                         pb.setProgress(downloadedSize);
@@ -111,12 +171,13 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
             }
-            //close the output stream when complete //
+            //close the output stream when complete
             fileOutput.close();
             runOnUiThread(new Runnable() {
                 public void run() {
                     Log.d("DUPA", "Plik pobrany");
                     // pb.dismiss(); // if you want close it..
+
                 }
             });
 
@@ -126,13 +187,13 @@ public class MainActivity extends AppCompatActivity {
         } catch (final IOException e) {
             showError("Error : IOException " + e);
             e.printStackTrace();
-        }
-        catch (final Exception e) {
+        } catch (final Exception e) {
             showError("Error : Please check your internet connection " + e);
         }
     }
 
-    void showError(final String err){
+
+    void showError(final String err) {
         runOnUiThread(new Runnable() {
             public void run() {
                 Toast.makeText(MainActivity.this, err, Toast.LENGTH_LONG).show();
@@ -140,6 +201,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 
     void showProgress(String file_path){
         dialog = new Dialog(MainActivity.this);
@@ -171,5 +233,100 @@ public class MainActivity extends AppCompatActivity {
                     REQUEST_EXTERNAL_STORAGE
             );
         }
+    }
+
+
+    /**
+     * This utility extracts files and directories of a standard zip file to
+     * a destination directory.
+     * @author www.codejava.net
+     *
+     */
+    public class UnzipUtility {
+        /**
+         * Size of the buffer to read/write data
+         */
+        private static final int BUFFER_SIZE = 4096;
+
+        /**
+         * Extracts a zip file specified by the zipFilePath to a directory specified by
+         * destDirectory (will be created if does not exists)
+         * @param zipFilePath
+         * @param destDirectory
+         * @throws IOException
+         */
+        private void unzip(String zipFilePath, String destDirectory) throws IOException
+        {
+            File destDir = new File(destDirectory);
+            if (!destDir.exists()) {
+                destDir.mkdir();
+            }
+            ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath));
+            ZipEntry entry = zipIn.getNextEntry();
+            // iterates over entries in the zip file
+            while (entry != null) {
+                String filePath = destDirectory + File.separator + entry.getName();
+                if (!entry.isDirectory()) {
+                    // if the entry is a file, extracts it
+                    extractFile(zipIn, filePath);
+                } else {
+                    // if the entry is a directory, make the directory
+                    File dir = new File(filePath);
+                    dir.mkdir();
+                }
+                zipIn.closeEntry();
+                entry = zipIn.getNextEntry();
+            }
+            zipIn.close();
+        }
+
+        /**
+         * Extracts a zip entry (file entry)
+         * @param zipIn
+         * @param filePath
+         * @throws IOException
+         */
+        private void extractFile(ZipInputStream zipIn, String filePath) throws IOException
+        {
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
+            byte[] bytesIn = new byte[BUFFER_SIZE];
+            int read = 0;
+            while ((read = zipIn.read(bytesIn)) != -1) {
+                bos.write(bytesIn, 0, read);
+            }
+            bos.close();
+        }
+    }
+
+    public boolean isInternetAvailable() {
+        try {
+            InetAddress ipAddr = InetAddress.getByName("google.com"); //You can replace it with your name
+            return !ipAddr.equals("");
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+
+    public boolean isServerOnline() {
+
+        Runtime runtime = Runtime.getRuntime();
+        try {
+
+            Process ipProcess = runtime.exec("/system/bin/ping -c 1 192.168.13.1");
+            int     exitValue = ipProcess.waitFor();
+            return (exitValue == 0);
+
+        } catch (IOException | InterruptedException e) { e.printStackTrace(); }
+
+        return false;
+    }
+
+
+    public static int pingHost(String host) throws IOException, InterruptedException {
+        String cmd = "/system/bin/ping -c 1 -W 1000 " + host;
+        Process proc = Runtime.getRuntime().exec(cmd);
+        proc.waitFor();
+        return proc.exitValue();
     }
 }
